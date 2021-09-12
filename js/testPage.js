6439ce02acbbd91d217641f86ec10bc304a8eb2f
Gianluca <graziadei.gianluca@gmail.com>
2021-09-12 19:23:42 +0200

    broadcast channel debug

--- js/testPage.js
+++ js/testPage.js
@@ -1,5 +1,10 @@
 const channel = new BroadcastChannel("greenPass-reader");
+
 channel.addEventListener("message", e => {
+    var printerArea = document.getElementById('printer');
     console.log(e.data);
+    var p = document.createElement('p');
+    p.textContent = JSON.stringify(e.data);
+    printerArea.appendChild(p);
 });
 
